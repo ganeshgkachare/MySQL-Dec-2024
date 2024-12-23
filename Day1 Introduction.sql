b577d841d7afdc594b7dd999114afe941eefe7b8
Ganesh G Kachare <100514201+ganeshgkachare@users.noreply.github.com>
2024-12-23 09:06:58 +0530

    Add files via upload

---
+++ Day1 Introduction.sql
@@ -0,0 +1,61 @@
+ 
+ --              Introduction  MySQL (SEQUEL)         -- 
+/*
+-- What is SQL?
+SQL stands for Structured Query Language. It is a standardized programming language designed for managing and manipulating
+relational databases. 
+SQL is used to interact with databases, including retrieving, updating, inserting, and deleting data. 
+SQL is the most widely used language for database management, especially with relational databases like MySQL, PostgreSQL, Oracle,
+and Microsoft SQL Server.
+
+-- What is MySQL?
+MySQL is an open-source Relational Database Management System (RDBMS) that uses SQL as its standard language
+ for interacting with databases. 
+
+-- Difference between SQL and MySQL
+SQL is a programming language used to manage relational databases, while 
+MySQL is a popular relational database management system that uses SQL as 
+its query language.
+
+
+-- Database
+A database is a structured collection of data, typically stored in tables, which can be managed efficiently.
+
+-- Database Management System (DBMS)  
+A Database Management System (DBMS) is system software for creating and managing databases. 
+There are two types of DBMS: 
+1) Relational Database Management System (RDBMS):
+ In RDBMS, the data is stored in relations (tables). Example – MySQL. 
+2) Non-Relational Database Management System (NoSQL):
+ It stores data in a non-tabular form (Graph).  Example – MongoDB 
+
+
+-- What can SQL do? 
+1. SQL can retrieve data from a database 
+2. SQL can insert records in a database 
+3. SQL can update records in a database 
+4. SQL can delete records from a database 
+5. SQL can create new databases 
+6. SQL can create new tables in a database 
+7. SQL can create stored procedures in a database 
+8. SQL can create views in a database 
+9. SQL can set permissions on tables, procedures, and views
+*/
+
+
+-- comment
+
+/*
+Multiline comment
+*/
+
+show databases;
+
+create database SQL1;
+show databases;
+
+create database SQL1;
+
+create database if not exists SQL1;
+
+Use mysql12;
